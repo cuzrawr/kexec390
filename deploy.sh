fc904fbfbf2098d86ca24b49ceffd4382ed317f3
cuzrawr <46599814+cuzrawr@users.noreply.github.com>
2024-10-07 19:31:25 +0300

    return to sh

--- deploy.sh
+++ deploy.sh
@@ -100,27 +100,10 @@ gather_ntwrk() {
 
 	cidr_to_netmask() {
 		local cidr=$1
-		local mask=""
-		local full_octets=$((cidr / 8))
-		local partial_octet=$((cidr % 8))
-
-		# Full 255 octets
-		for ((i=0; i<full_octets; i++)); do
-			mask+=255
-			[[ $i -lt 3 ]] && mask+=.
-		done
-
-		# Partial octet
-		if [[ $full_octets -lt 4 ]]; then
-			local octet=$((256 - 2**(8-partial_octet)))
-			mask+=$octet
-			for ((i=full_octets+1; i<4; i++)); do
-				mask+=.0
-			done
-		fi
-
-		echo $mask
+		local value=$(( 0xffffffff ^ ((1 << (32 - cidr)) - 1) ))
+		echo "$(( (value >> 24) & 0xff )).$(( (value >> 16) & 0xff )).$(( (value >> 8) & 0xff )).$(( value & 0xff ))"
 	}
+
 
 	# Get default iface
 	IFNM=$(ip -c=never -o link show | grep -oP '(?<=^2:\s)\w+')
